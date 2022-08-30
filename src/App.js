9c8ef2a86dfea8cf70a7bf55af0cd9a46d77920f
ChangHui Han <xmflr95@naver.com>
2022-08-30 18:01:40 +0900

    curr study

--- src/App.js
+++ src/App.js
@@ -1,9 +1,80 @@
-import React from "react";
-import Counter from "./Counter";
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {    
+    const { name, value } = e.target;    
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    },
+  ]);
+  
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id != id));
+  }
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
-    <Counter />
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={ users } onRemove={onRemove} onToggle={onToggle} />
+    </>
   );
 }
 
