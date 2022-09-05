10aaac3c5a989838e43fd5aa5ba20c1fac62ee9a
ChangHui Han <xmflr95@naver.com>
2022-09-05 16:04:45 +0900

    custom Hook and useContext API

--- src/App.js
+++ src/App.js
@@ -36,18 +36,10 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,  // input 초기화
-        users: state.users.concat(action.user)
+        // inputs: initialState.inputs,  // input 초기화
+        users: state.users.concat(action.user)        
       };
     case 'TOGGLE_USER':
       return {
@@ -66,133 +58,21 @@ function reducer(state, action) {
   }
 }
 
+export const UserDispatch = React.createContext(null);
+
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
-  
+  const [state, dispatch] = useReducer(reducer, initialState); 
+
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  // const [inputs, setInputs] = useState({
-  //   username: '',
-  //   email: '',
-  // });
-  // const { username, email } = inputs;
-
-  // const onChange = useCallback(e => {
-  //   const { name, value } = e.target;    
-  //   setInputs(inputs => ({
-  //     ...inputs,
-  //     [name]: value
-  //   }));
-  // }, []);
-  // const [users, setUsers] = useState([
-  //   {
-  //     id: 1,
-  //     username: 'velopert',
-  //     email: 'public.velopert@gmail.com',
-  //     active: true
-  //   },
-  //   {
-  //     id: 2,
-  //     username: 'tester',
-  //     email: 'tester@example.com',
-  //     active: false
-  //   },
-  //   {
-  //     id: 3,
-  //     username: 'liz',
-  //     email: 'liz@example.com',
-  //     active: false
-  //   },
-  // ]);
   
-  // const nextId = useRef(4);
-  // const onCreate = useCallback(() => {
-  //   const user = {
-  //     id: nextId.current,
-  //     username,
-  //     email
-  //   };
-  //   // setUsers([...users, user]);
-  //   setUsers(users => users.concat(user)); // 함수형 업데이트
-
-  //   setInputs({
-  //     username: '',
-  //     email: ''
-  //   });
-  //   nextId.current += 1;
-  // }, [username, email]);
-
-  // const onRemove = useCallback
-  // (
-  //     id => {
-  //     setUsers(users => users.filter(user => user.id != id));
-  //   }, 
-  //   [users]
-  // );
-
-  // const onToggle = useCallback(
-  //   id => {
-  //     setUsers(users => 
-  //       users.map(user => 
-  //         user.id === id ? { ...user, active: !user.active } : user
-  //       )
-  //     );
-  // }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    });
-  }, []);
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
   
   return (
-    <>
-      <CreateUser 
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList 
-        users={ users } 
-        onRemove={onRemove} 
-        onToggle={onToggle} 
-      />
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={ users } />
       <div>활성사용자 수 : {count}</div>      
-    </>
+    </UserDispatch.Provider>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { UserDispatch } from './App';
 
-const User = React.memo(function User({ user, onRemove, onToggle }) {
-  // useEffect(() => {
-  //   console.log('user 값이 설정됨');
-  //   console.log(user);
-  //   return () => {
-  //     console.log('user 가 바뀌기 전...');
-  //     console.log(user);
-  //   }
-  // }, [user]);
-  // useEffect(() => console.log(user));  
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
       <b
@@ -17,27 +11,29 @@ const User = React.memo(function User({ user, onRemove, onToggle }) {
           cursor: 'pointer',
           color: user.active ? 'green' : 'black'
         }} 
-        onClick={() => onToggle(user.id)}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
       >
         {user.username}
       </b>
       &nbsp;
       <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
+      <button onClick={() => {
+        dispatch({ type: 'REMOVE_USER', id: user.id });
+      }}
+      >
+        삭제
+      </button>
     </div>
   );
 });
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
   return (
     <div>
       {users.map(user => (
-        <User 
-          user={user} 
-          key={user.id} 
-          onRemove={onRemove} 
-          onToggle={onToggle} 
-        />
+        <User user={user} key={user.id} />
       ))}
     </div>
   )
